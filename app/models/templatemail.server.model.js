58ea8c8f4c0b8068ae00da57e39311d47ac795da
unknown <opinto@hxnb-asus01.hexacta.com>
2015-05-12 17:42:21 -0300

    se grega cuerpo al mail

--- app/models/templatemail.server.model.js
+++ app/models/templatemail.server.model.js
@@ -10,11 +10,20 @@ var mongoose = require('mongoose'),
  * Templatemail Schema
  */
 var TemplatemailSchema = new Schema({
-	name: {
+	asunto: {
 		type: String,
 		default: '',
-		required: 'Please fill Templatemail name',
+		required: 'Por favor complete el asunto del mail',
 		trim: true
+	},
+	cuerpo: {
+		type: String,
+		default: '',
+		required: 'Por favor complete el cuerpo del mail',
+		trim: true
+	},
+	destinatarios: {
+		type: [String]
 	},
 	created: {
 		type: Date,
