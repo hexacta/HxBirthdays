31ac97e9363f65a8d06930bf8bd37d4dc83a1caf
unknown <jtaira@hxws-jtaira.hexacta.com>
2015-05-19 08:38:31 -0300

    Se agrega condition para el link HxBirthday. Se sacan los links cuando se está deslogueado. Se corrije el link de Usuarios.

--- public/modules/usuarios/config/usuarios.client.config.js
+++ public/modules/usuarios/config/usuarios.client.config.js
@@ -4,6 +4,6 @@
 angular.module('usuarios').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'usuarios', 'Lista de Usuarios', 'usuarios');
+		Menus.addMenuItem('topbar', 'Usuarios', 'usuarios', ' ', 'usuarios');
 	}
 ]);
