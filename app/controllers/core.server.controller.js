a9c69b12c5fba0590be871e078e5ead46fd7b0ed
unknown <jtaira@hxws-jtaira.hexacta.com>
2015-05-26 16:13:08 -0300

    Se modifica la vista de configure friends.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -6,7 +6,8 @@
 var mongoose = require('mongoose'),
 	errorHandler = require('./errors.server.controller'),
 	User = mongoose.model('User'),
-	_ = require('lodash');
+	_ = require('lodash'),
+	fs = require('fs');
 
 exports.index = function(req, res) {
 	res.render('index', {
@@ -15,20 +16,44 @@ exports.index = function(req, res) {
 	});
 };
 
+// Lectura de archivo
+var theLDAPUrl;
+var user;
+var pass;
+var dn;
+var params = {};
+
+// Lectura de archivo
+var fileData = fs.readFileSync('LDAP/ldap_params.txt', 'utf8');
+var fileLines = fileData.split('\r\n');
+
+for (var x in fileLines){
+	var fileLine = fileLines[x].split('|');
+	params[fileLine[0]] = fileLine[1];
+}
+// Lectura de archivo
+
+theLDAPUrl = params.url;
+user = params.user;
+pass = params.pass;
+dn = params.dn;
+
 //Scheduler create users 
 var schedule = require('node-schedule');
 
-var j = schedule.scheduleJob('0 0 * * *', function(){ 
-	//Ldap
-	var ldap = require('ldapjs');
+var j = schedule.scheduleJob('0 0 * * *', function(){
 
-	ldap.Attribute.settings.guid_format = ldap.GUID_FORMAT_B;
+//Ldap
+var ldap = require('ldapjs');
+
+ldap.Attribute.settings.guid_format = ldap.GUID_FORMAT_B;
+
 
 	var client = ldap.createClient({
-	  url: 'la_url'
+	  url: theLDAPUrl
 	});
 
-	client.bind('usuario', 'clave', function(err) {
+	client.bind(user, pass, function(err) {
 	  	console.log('Conectado a LDAP!');
 	});
 
@@ -37,7 +62,7 @@ var j = schedule.scheduleJob('0 0 * * *', function(){
 	};
 
 	// Se usa distinguishedName para entrar
-	client.search('el_dn', opts, function(err, res) {  
+	client.search(dn, opts, function(err, res) {  
 		var lista = res.on('searchEntry', function(entry) {
 			User.find().where('username').equals(entry.object.name).exec(function(err, users) {
 				// Si no lo encuentro, lo agrego
@@ -48,7 +73,6 @@ var j = schedule.scheduleJob('0 0 * * *', function(){
 			 		newUser.displayName = entry.object.givenName +' ' + entry.object.sn;
 			 		newUser.email = entry.object.mail;
 			 		newUser.username = entry.object.name;
-			 		newUser.photo = 'la_foto'
 			 		newUser.birthday = '8/6/1984';
 			 		newUser.salt = null;
 			 		newUser.password = null;
