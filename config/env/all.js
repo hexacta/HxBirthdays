b7e0b597ab2ce0ab13f2af5b04f40602ec4a5032
unknown <jtaira@hxws-jtaira.hexacta.com>
2015-06-05 10:08:51 -0300

    Se agregan las relaciones para que se cierre el mensaje de error con un boton.

--- config/env/all.js
+++ config/env/all.js
@@ -25,7 +25,11 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/jquery/dist/jquery.js',
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/bootstrap/dist/js/bootstrap.js',
+				'public/lib/bootstrap/dist/js/bootstrap.min.js'
 			]
 		},
 		css: [
