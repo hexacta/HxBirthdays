31e03534f53c81ccc2864ea44fbb2324e4ad2961
unknown <mrios@hxws-mrios.hexacta.com>
2015-06-25 17:35:26 -0300

    Reedicion del Controller para modularizar

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,11 +3,9 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-	errorHandler = require('./errors.server.controller'),
-	User = mongoose.model('User'),
-	_ = require('lodash'),
-	fs = require('fs');
+ var mongoose = require('mongoose'),
+	 User = mongoose.model('User'),
+	 obtenerUsuario = require('./users/users.schedule.server.controller');
 
 exports.index = function(req, res) {
 	res.render('index', {
@@ -16,75 +14,6 @@ exports.index = function(req, res) {
 	});
 };
 
-// Lectura de archivo
-var theLDAPUrl;
-var user;
-var pass;
-var dn;
-var params = {};
-
-// Lectura de archivo
-/*
-* TODO: Meter esto en un servicio.
-*/
-var fileData = fs.readFileSync('LDAP/ldap_params.txt', 'utf8');
-var fileLines = fileData.split('\r\n');
-
-for (var x in fileLines){
-	var fileLine = fileLines[x].split('|');
-	params[fileLine[0]] = fileLine[1];
-}
-// Lectura de archivo
-
-theLDAPUrl = params.url;
-user = params.user;
-pass = params.pass;
-dn = params.dn_bs + ',' + params.dn;
-
-//Scheduler create users 
-var schedule = require('node-schedule');
-
-var j = schedule.scheduleJob('0 0 * * *', function(){
-
-//Ldap
-var ldap = require('ldapjs');
-
-ldap.Attribute.settings.guid_format = ldap.GUID_FORMAT_B;
 
 
-	var client = ldap.createClient({
-	  url: theLDAPUrl
-	});
-
-	client.bind(user, pass, function(err) {
-	  	console.log('Conectado a LDAP!');
-	});
-
-	var opts = {
-	  scope: 'one'
-	};
-
-	// Se usa distinguishedName para entrar
-	client.search(dn, opts, function(err, res) {
-		var lista = res.on('searchEntry', function(entry) {
-
-			User.findOne().where('username').equals(entry.object.name).exec(function(err, users) {
-				// Si no lo encuentro, lo agrego
-				if (!users) {
-					var newUser = new User();
-			 		newUser.firstName = entry.object.givenName;
-			 		newUser.lastName = entry.object.sn;
-			 		newUser.displayName = entry.object.givenName +' ' + entry.object.sn;
-			 		newUser.email = entry.object.mail;
-			 		newUser.username = entry.object.name;
-			 		newUser.birthday = '8/6/1984';
-			 		newUser.salt = null;
-			 		newUser.password = null;
-			 		newUser.usersFriends = [];
-
-			 		newUser.save();
-				}
-			});
-		});
-	});
-});
+obtenerUsuario.registroDeUsuarios();
--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -7,64 +7,26 @@ var _ = require('lodash'),
 	errorHandler = require('../errors.server.controller'),
 	mongoose = require('mongoose'),
 	passport = require('passport'),
-	User = mongoose.model('User'),
-	ldap = require('ldapjs'),
-	fs = require('fs');
-
-var theLDAPUrl;
-var params = {};
-
-// Lectura de archivo
-var fileData = fs.readFileSync('LDAP/ldap_params.txt', 'utf8');
-var fileLines = fileData.split('\r\n');
-
-for (var x in fileLines){
-	var fileLine = fileLines[x].split('|');
-	params[fileLine[0]] = fileLine[1];
-}
-theLDAPUrl = params.url;
+	User = mongoose.model('User');
+var	logueo = require('./users.bindAuthentication.server');
 
 /**
  * Signin after passport authentication
  */
 exports.signin = function(req, res, next) {
-
-	var client = ldap.createClient({
-		      url: theLDAPUrl
-	});
-
-	client.bind(String(req.body.username) + '@hexacta.com', String(req.body.password), function(err) {
-		if(!err){			
-			User.findOne().where('username').equals(req.body.username).exec(function(err, usuario) {
-				if (!err) {
-					req.login(usuario, function(err) {
-						if (err) {
-							res.status(400).send(err);
-						} else {
-							res.json(usuario);
-						}	
-					});
-				}
-			});
-		} else {
-			res.status(400).send({message: 'Usuario o Clave Incorrecta'});
-		}
-	});
-};
+	res=logueo.loginLDAP(req,res);
+	};
 	
 
 /**
  * Signout
  */
 exports.signout = function(req, res) {
-
 	req.logout();
-
 	passport.deserializeUser(function(id, done) {
 	  User.findById(id, function(err, user) {
 	    done(err, user);
 	  });
 	});
-
 	res.redirect('/');
 };
