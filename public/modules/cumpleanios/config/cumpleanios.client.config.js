1c3a2ca1e1eddbf533f6ed42867f32da0217d63c
unknown <mrios@hxws-mrios.hexacta.com>
2015-06-26 11:54:09 -0300

    Caracteres especiales en JS

--- public/modules/cumpleanios/config/cumpleanios.client.config.js
+++ public/modules/cumpleanios/config/cumpleanios.client.config.js
@@ -4,9 +4,9 @@
 angular.module('cumpleanios').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Cumplea&ntilde;os', 'cumpleanios', 'dropdown', '/cumpleanios(/create)?');
-		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Lista de Cumplea&ntilde;os', 'cumpleanios');
-		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Nuevo Cumplea&ntilde;o', 'cumpleanios/create');
-		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Para estos no se junta todav&iacute;a', 'cumplanios-no-colectados');
+		Menus.addMenuItem('topbar', 'Cumplea\u00f1os', 'cumpleanios', 'dropdown', '/cumpleanios(/create)?');
+		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Lista de Cumplea\u00f1os', 'cumpleanios');
+		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Nuevo Cumplea\u00f1o', 'cumpleanios/create');
+		Menus.addSubMenuItem('topbar', 'cumpleanios', 'Para estos no se junta todav\u00eda', 'cumplanios-no-colectados');
 	}
 ]);
